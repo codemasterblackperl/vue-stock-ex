94eecffd5f444ee4171a063171baee341b5f08fa
Ajit <codemasterblackperl@outlook.com>
2018-05-31 21:45:46 +0530

    routing is working and also added bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+//router
+import VueRouter from "vue-router";
+import {
+  routes
+} from "./routes";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes
+});
+
+//bootstrap
+import Bootstrap from "bootstrap-vue";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(Bootstrap);
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
